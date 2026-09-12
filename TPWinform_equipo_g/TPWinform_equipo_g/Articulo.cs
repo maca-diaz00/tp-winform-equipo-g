13130fb35e078e143e3199872f57fc2b43d3860f
J. Macarena Diaz <macarenadiaz0028@gmail.com>
2026-09-11 21:07:30 -0300

    Creacion de clases principales
    Articulo=incluyendo atributos de tipo Imagen, Categoria y marca

---
+++ TPWinform_equipo_g/TPWinform_equipo_g/Articulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinform_equipo_g
+{
+    internal class Articulo
+    {
+        public int Id { get; set; }
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public List<Imagen> Imagenes { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+
+    }
+}
---
+++ TPWinform_equipo_g/TPWinform_equipo_g/Imagen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinform_equipo_g
+{
+    internal class Imagen
+    {
+        public int Id { get; set; }
+        public string UrlImagen { get; set; }
+    }
+}
