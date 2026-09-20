e911e32c331b4d38c768559fb66d125589a51f2f
francolav <francolavarda@hotmail.com>
2026-09-20 15:05:07 -0300

    .

--- TPWinform_equipo_g/TPWinform_equipo_g/frmPrincipal.Designer.cs
+++ TPWinform_equipo_g/TPWinform_equipo_g/frmPrincipal.Designer.cs
@@ -692,7 +692,6 @@
             this.txtFiltroBaseDatos.Size = new System.Drawing.Size(100, 20);
             this.txtFiltroBaseDatos.TabIndex = 34;
             this.txtFiltroBaseDatos.Visible = false;
-            this.txtFiltroBaseDatos.TextChanged += new System.EventHandler(this.txtFiltroBaseDatos_TextChanged);
             // 
             // frmPrincipal
             // 
